2fa162008bd617ba58263eb95b55258515253cd4
fefferico <fefferico@gmail.com>
2025-06-18 15:50:21 +0200

    Auto-generated commit

---
+++ chunk-MCJNTTNO.js
@@ -0,0 +1,1 @@
+import{c as n,d as a}from"./chunk-PWJEGVQU.js";import{Cc as i,Dc as f}from"./chunk-U2DZZCFG.js";function m(r,t,e){let o=f(r,e?.in);return isNaN(t)?i(e?.in||r,NaN):(t&&o.setDate(o.getDate()+t),o)}function x(r,t,e){let[o,s]=n(e?.in,r,t);return+a(o)==+a(s)}function N(r,t,e){return m(r,-t,e)}export{m as a,x as b,N as c};
