a9e1efc4be4641850effaca3775008804ccdc8e7
fefferico <fefferico@gmail.com>
2025-07-18 00:38:01 +0200

    Auto-generated commit

---
+++ chunk-2QBRPWLY.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-JPLHZ5LI.js";import{a as n}from"./chunk-OGJDFM7O.js";import{Sc as i,Tc as f}from"./chunk-ZE5GR7Q4.js";function m(r,t,e){let o=f(r,e?.in);return isNaN(t)?i(e?.in||r,NaN):(t&&o.setDate(o.getDate()+t),o)}function x(r,t,e){let[o,s]=n(e?.in,r,t);return+a(o)==+a(s)}function N(r,t,e){return m(r,-t,e)}export{m as a,x as b,N as c};
