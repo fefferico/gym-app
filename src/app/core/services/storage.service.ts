f49c434fb91dee248f75dc4d0702dbca5ae82282
fefferico <fefferico@gmail.com>
2025-08-20 18:56:41 +0200

    updated version

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -9,7 +9,7 @@ export class StorageService {
   private isBrowser: boolean;
   private alertService = inject(AlertService);
 
-  private version = '1.0.2'; // Version of the storage service
+  private version = '1.0.3'; // Version of the storage service
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     this.isBrowser = isPlatformBrowser(this.platformId);
