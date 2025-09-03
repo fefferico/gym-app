0c87bd1eab7d83a339273c8ab4e48e37fe2e7cdd
fefferico <fefferico@gmail.com>
2025-09-03 12:20:38 +0200

    Auto-generated commit

--- assets/icons/icon-data.ts
+++ assets/icons/icon-data.ts
@@ -15,6 +15,11 @@ export const ICONS: Record<string, string> = {
       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-11.25a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z" clip-rule="evenodd" />
     </svg>
   `,
+  'minus-circle': `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM6.75 9.25a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z" clip-rule="evenodd" />
+</svg>
+  `,
 
   // Add all your other icons here...
   'trash': `
