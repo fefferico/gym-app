e08c32bf9b798008a526d5246d58b6cb591713e5
fefferico <fefferico@gmail.com>
2025-09-28 17:03:26 +0200

    Auto-generated commit

--- assets/icons/icon-data.ts
+++ assets/icons/icon-data.ts
@@ -552,4 +552,12 @@ viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" >
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
 </svg>`,
+'right-arrow':`
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+</svg>`,
+'left-arrow':`
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+</svg>`,
 };
